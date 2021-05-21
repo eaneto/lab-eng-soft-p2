35f00a7b65fe318cd734f17c82d4614a92c3f414
Edison Neto <edison.aguiar.neto@gmail.com>
2021-05-20 21:08:07 -0300

    Update example app

--- example-app/app.go
+++ example-app/app.go
@@ -1,8 +1,42 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/eaneto/notify"
+	sqs "github.com/eaneto/notify/service/amazonsqs"
+)
 
 func sqsHandler(w http.ResponseWriter, r *http.Request) {
+	notifier := notify.New()
+	fmt.Println(notifier)
+
+	// Create a telegram service. Ignoring error for demo simplicity.
+	sqsService, err := sqs.New("accesskeyid", "secretkey", "region")
+
+	if err != nil {
+		fmt.Println("Yes, error")
+	}
+
+	// Passing a telegram chat id as receiver for our messages.
+	// Basically where should our message be sent?
+	sqsService.AddReceivers("http://definetly-not-the-actual-sqs-endpoint.com")
+
+	// Tell our notifier to use the telegram service. You can repeat the above process
+	// for as many services as you like and just tell the notifier to use them.
+	// Inspired by http middlewares used in higher level libraries.
+	notifier.UseServices(sqsService)
+
+	// Send a test message.
+	error := notifier.Send(
+		context.Background(),
+		"Subject",
+		"Example message",
+	)
+
+	fmt.Println(error)
 }
 
 func snsHandler(w http.ResponseWriter, r *http.Request) {
--- example-app/go.mod
+++ example-app/go.mod
@@ -1,3 +1,14 @@
 module gitihub.com/lab-end-soft-p2/example-app
 
 go 1.16
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v1.2.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.4.0 // indirect
+	github.com/eaneto/notify v0.17.1 // indirect
+	github.com/nikoksr/notify v0.16.1 // indirect
+)
+
+replace github.com/nikiksr/notify v0.16.1 => github.com/eaneto/notify v0.17.1
+
+replace github.com/nikiksr/notify/amazonsqs v0.16.1 => github.com/eaneto/notify/amazonsqs v0.17.1
