6b0162a804239652e676ba2ecb34ed3eeb876d84
raphaelabrantes <antenabr2@gmail.com>
2021-05-22 22:37:48 -0300

    Added the snsHandler and error handling

--- example-app/app.go
+++ example-app/app.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/eaneto/notify"
+	sns "github.com/eaneto/notify/service/amazonsns"
 	sqs "github.com/eaneto/notify/service/amazonsqs"
 	"github.com/sirupsen/logrus"
 )
@@ -46,6 +47,35 @@ func sqsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func snsHandler(w http.ResponseWriter, r *http.Request) {
+	notifier := notify.New()
+	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	region := os.Getenv("AWS_DEFAULT_REGION")
+	topic := os.Getenv("AWS_SNS_TOPIC")
+	account := os.Getenv("AWS_ACCOUNT")
+
+	snsService, err := sns.New(accessKey, secretKey, region)
+	if err != nil {
+		logrus.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	baseTopic := "arn:aws:sns:%s:%s:%s"
+	snsService.AddReceivers(fmt.Sprintf(baseTopic, region, account, topic))
+	notifier.UseServices(snsService)
+
+	err = notifier.Send(
+		context.Background(),
+		"Subject",
+		"Example message")
+
+	if err != nil {
+		logrus.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		logrus.Info("Message published succesfully")
+		w.WriteHeader(http.StatusOK)
+	}
 }
 
 // healthHandler returns ok always, used by nagios.
@@ -58,5 +88,9 @@ func main() {
 	http.HandleFunc("/publish-sns", snsHandler)
 	http.HandleFunc("/health", healthHandler)
 
-	http.ListenAndServe(":8888", nil)
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		logrus.Error("Failed to Listen and Serve")
+		return
+	}
 }
